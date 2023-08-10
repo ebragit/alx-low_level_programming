c18c290b2729ec9d8ef5d02f904cf43a53fb7d42
ebragit <outorinside@gmail.com>
2023-08-10 16:41:49 -0700

    edit 101-multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,76 +1,95 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdio.h>
 
-// Function to check if a string consists of only digits
-int isNumber(const char *str) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (!isdigit(str[i])) {
-            return 0;
-        }
-    }
-    return 1;
+#define ERR_MSG "Error"
+
+/**
+ * is_number - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_number(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-// Function to multiply two strings representing positive integers
-char *multiplyStrings(const char *num1, const char *num2) {
-    int len1 = strlen(num1);
-    int len2 = strlen(num2);
-    int len3 = len1 + len2;
-    
-    char *result = (char *)malloc(sizeof(char) * (len3 + 1));
-    if (!result) {
-        perror("Memory allocation error");
-        exit(1);
-    }
-    memset(result, '0', len3);
-    result[len3] = '\0';
-    
-    for (int i = len1 - 1; i >= 0; i--) {
-        int carry = 0;
-        for (int j = len2 - 1; j >= 0; j--) {
-            int product = (num1[i] - '0') * (num2[j] - '0') + carry + (result[i + j + 1] - '0');
-            carry = product / 10;
-            result[i + j + 1] = (product % 10) + '0';
-        }
-        result[i] += carry;
-    }
-    
-    return result;
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Error\n");
-        return 98;
-    }
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-    if (!isNumber(argv[1]) || !isNumber(argv[2])) {
-        printf("Error\n");
-        return 98;
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_number(s1) || !is_number(s2))
+    {
+		printf("Error\n");
+	    exit(98);
     }
-
-    char *num1 = argv[1];
-    char *num2 = argv[2];
-
-    // Remove leading zeros
-    while (*num1 == '0') {
-        num1++;
-    }
-    while (*num2 == '0') {
-        num2++;
-    }
-
-    if (*num1 == '\0' || *num2 == '\0') {
-        printf("0\n");
-        return 0;
-    }
-
-    char *result = multiplyStrings(num1, num2);
-    printf("%s\n", result);
-    
-    free(result);
-    
-    return 0;
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+        _putchar('0');
+        _putchar('\n');
+        free(result);
+	return (0);
 }
