8bc05b720e1442a4c06a22eca2db20860c392a5e
ebragit <outorinside@gmail.com>
2023-08-28 09:49:48 -0700

    fix 9-insert_node_int

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,50 +1,45 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at
- *                           a given position
+ * insert_nodeint_at_index - Inserts a new node at a given position in a list.
+ * @head: Double pointer to the head of the list.
+ * @idx: Index of the list where the new node should be added (starting at 0).
+ * @n: Value to be assigned to the new node's data.
  *
- * @head: pointer to the first node of the list
- * @idx: is the index of the list where the new node should be added
- * @n: element to add to the new node
- *
- * Return: NULL if anything fails or the address of the new node
-*/
+ * Return: Address of the new node, or NULL if it failed.
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current;
-	unsigned int index;
+    listint_t *new_node, *current = *head;
+    unsigned int count = 0;
 
-	current = *head;
+    if (head == NULL || idx == 0)
+    {
+        new_node = malloc(sizeof(listint_t));
+        if (new_node == NULL)
+            return (NULL);
+        new_node->n = n;
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
 
-	new_node = malloc(sizeof(listint_t));
-	if ((*head == NULL && idx != 0) || new_node == NULL)
-		return (NULL);
+    while (current != NULL && count < idx - 1)
+    {
+        current = current->next;
+        count++;
+    }
 
-	new_node->n = n;
+    if (current == NULL)
+        return (NULL);
 
-	for (index = 0; head != NULL && index < idx - 1; index++)
-	{
-		current = current->next;
-		if (current == NULL)
-			return (NULL);
-	}
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-	}
-	else if (current->next)
-	{
-		new_node->next = current->next; 
-		current->next = new_node;
-	}
-	else 
-	{
-		new_node->next = NULL;
-		current->next = new_node;
-	}
+    new_node->n = n;
+    new_node->next = current->next;
+    current->next = new_node;
 
-	return (new_node);
-}
+    return (new_node);
+}}
