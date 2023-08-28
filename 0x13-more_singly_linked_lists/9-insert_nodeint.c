bbb3fba417c111c7170a9db67848154302f76f59
ebragit <outorinside@gmail.com>
2023-08-28 10:59:38 -0700

    fix I swear 9-insert_node_int

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,45 @@
-#include "lists.h"
-
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position in a list.
- * @head: Double pointer to the head of the list.
- * @idx: Index of the list where the new node should be added (starting at 0).
- * @n: Value to be assigned to the new node's data.
+ * insert_nodeint_at_index - function with 3 arguments
+ * @head: pointer to head pointer of first node in linked list
+ * @idx: index of list
+ * @n: value integer
  *
- * Return: Address of the new node, or NULL if it failed.
+ * Description: inserts a new node at a given position
+ * Return: address of the new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        listint_t *new_node, *current = *head;
-        unsigned int count = 0;
+	listint_t *temp, *cursor;
+	unsigned int count = 0;
 
-        if (head == NULL || idx == 0)
-        {
-        new_node = malloc(sizeof(listint_t));
-        if (new_node == NULL)
-                return (NULL)
-        new_node->n = n;
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-        }
+	if (head == NULL)
+		return (NULL);
 
-        while (current != NULL && count < idx - 1)
-        {
-        current = current->next;
-        count++;
-        }
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->n = n;
+	cursor = *head;
 
-        if (current == NULL)
-        return (NULL);
+	if (idx == 0)
+	{
+		temp->next = *head;
+		*head = temp;
+		return (*head);
+	}
 
-        new_node = malloc(sizeof(listint_t));
-        if (new_node == NULL)
-        return (NULL);
+	while (cursor != NULL)
+	{
+		if (count == idx - 1)
+		{
+			temp->next = cursor->next;
+			cursor->next = temp;
+		}
+		count++;
+		cursor = cursor->next;
+	}
+	if (idx > count)
+		return (NULL);
+	return (temp);
 
-        new_node->n = n;
-        new_node->next = current->next;
-        current->next = new_node;
-
-        return (new_node);
 }
