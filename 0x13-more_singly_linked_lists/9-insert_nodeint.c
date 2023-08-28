693de5577657e80affd04a842f7ae85179a91a36
ebragit <outorinside@gmail.com>
2023-08-28 09:59:43 -0700

    fix 9-insert_node_int typo

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,36 +10,36 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new_node, *current = *head;
-    unsigned int count = 0;
+	listint_t *new_node, *current = *head;
+	unsigned int count = 0;
 
-    if (head == NULL || idx == 0)
-    {
-        new_node = malloc(sizeof(listint_t));
-        if (new_node == NULL)
-            return (NULL);
-        new_node->n = n;
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
+	if (head == NULL || idx == 0)
+	{
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL)
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+	}
 
-    while (current != NULL && count < idx - 1)
-    {
-        current = current->next;
-        count++;
-    }
+	while (current != NULL && count < idx - 1)
+	{
+	current = current->next;
+	count++;
+	}
 
-    if (current == NULL)
-        return (NULL);
+	if (current == NULL)
+	return (NULL);
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	return (NULL);
 
-    new_node->n = n;
-    new_node->next = current->next;
-    current->next = new_node;
+	new_node->n = n;
+	new_node->next = current->next;
+	current->next = new_node;
 
-    return (new_node);
-}}
+	return (new_node);
+}
